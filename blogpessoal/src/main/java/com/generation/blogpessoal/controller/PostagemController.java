5ea048028e00fcd56490ca4b1e095bb8182584ca
nathaliebfm <natibfm@hotmail.com>
2022-10-03 13:14:44 -0300

    Inclusão dos métodos get, put, delete e post no controller do blog pessoal

--- blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,13 +1,24 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
@@ -22,9 +33,58 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
 	
+	//Quando é getall não precisa identificar o endpoint, pois não tem filtro
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
+	
+	//ResponseEntity é uma requisição da tabela Postagens
+	@GetMapping("/{id}") //entre parênteses por ser identificador único
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+	    
+	    //-> (lambda) é um método dentro de outro método que não precisa configurar parâmetros
+	    return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+	            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build()); //build valida o que foi feito
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+	    
+	    return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	    
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+	    
+	    return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	    
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
+	    return postagemRepository.findById(postagem.getId())
+	            .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+	                    .body(postagemRepository.save(postagem)))
+	            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+	    
+	    Optional<Postagem> postagem = postagemRepository.findById(id);
+	    
+	    if(postagem.isEmpty())
+	        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	    
+	    postagemRepository.deleteById(id);
+	    
+	}
 
 }
+
+
+
+
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,17 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    
+    //É uma regra específica para o negócio, não é um padrão como no controller, então precisa criar primeiro aqui para usar lá
+    public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
